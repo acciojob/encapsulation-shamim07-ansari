4bb12787e9e66a10dbeb03684420fa379bad5855
Shamim <shamim73124@gmail.com>
2023-04-15 13:11:31 +0530

    Encapsulation code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Shamim");
+        String name = rwOnly.getName();
+        System.out.println(name);
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
